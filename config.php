e7603386f01913952795bfb54076ee221845f9bc
orliesaurus <orliesaurus@facebook.com>
2014-04-23 17:33:25 +0100

    few more fixes

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 //Find the api and secretkey here https://app.mailjet.com/account/api_keys
-$APIKey = '';
-$secretKey = '';
+$APIKey = 'API_KEY_GOES_HERE';
+$secretKey = 'SECRET_KEY_HERE';
+//Go to https://app.mailjet.com/contacts/lists and create an (empty) list. Find the ID by hovering the ? icon within the list's view page. In my example its 16
 $mylist = 16;
 ?>
